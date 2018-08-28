80838a62fec2ffd1110a2ff4e51e61007a4aaf09
Alex Jivov <alexjivov@gwu.edu>
2018-08-28 15:53:17 -0400

    initial burger.js build out

---
+++ public/assets/js/burger.js
@@ -0,0 +1,50 @@
+$(function() {
+    $(".change-devour").on("click",function(event) {
+        var id = $(this).data("id");
+
+        var newState = {
+            devoured: true
+        };
+
+        //PUT request
+        $.ajax("/api/burgers" + id, {
+            type: "PUT",
+            data: newState
+        }).then(function() {
+            location.reload();
+        });
+    });
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var name = $("[name=burger-name]").val().trim();
+
+        if(name !== "") {
+            var newBurger = {
+                name: name
+            };
+
+            //Sends POST request
+            $.ajax("/api/burgers", {
+                type: "POST",
+                data: newBurger
+            }).then(function() {
+                console.log("Created new burger");
+                location.reload();
+            });
+        }
+        else {
+            $("[name=buger-name]").val("");
+        }
+    });
+    $(".delete-sleep").on("click",function(event) {
+        var id = $(this).data("id");
+
+        $.ajax("/api/burgers" + id, {
+            type: "DELETE"
+        }).then(function() {
+            location.reload();
+        });
+    });
+});
