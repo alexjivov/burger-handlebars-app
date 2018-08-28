6843019eb457b518849a82e479ee963bbcce6b45
Alex Jivov <alexjivov@gwu.edu>
2018-08-28 15:31:24 -0400

    connection.js initial build out

---
+++ config/connection.js
@@ -0,0 +1,30 @@
+// mySQL connection
+var mysql = require("mysql");
+
+var connection;
+
+//set up mySQL
+if(process.env.JASWDB_URL) {
+    connection = mysql.createConnection(process.env.JASWDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,    
+        user: "root",
+        password: "y685d90c",
+        database: "burgers_db"
+    });
+}
+
+//Make the connection
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+//Export for ORM
+module.exports = connection; 
