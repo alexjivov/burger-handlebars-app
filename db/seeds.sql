388ea7040d03898bc0dc7bcc848133797d5c5087
Alex Jivov <alexjivov@gwu.edu>
2018-08-28 14:25:55 -0400

    creaated DB and seeds mysql burger DB

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Stuffed Lamb Burger');
+
