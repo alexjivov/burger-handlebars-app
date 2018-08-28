fe67a186cf7a308b13c06efa5a0271adaf2a15ac
Alex Jivov <alexjivov@gwu.edu>
2018-08-28 11:12:28 -0400

    initial server.js buildout complete

---
+++ server.js
@@ -0,0 +1,32 @@
+// dependencies 
+var express = require("express");
+var bodyParser = require("body-parser");
+
+//specifying port
+var port = process.env.PORT || 3000;
+
+//initializing app
+var app = exprsss();
+
+// static content for the app from the directory - public -> app directory
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Handlebars dependencies
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: main}));
+app.set("view engine", "handlebars");
+
+//ROUTES import
+var routes = require("./controllers/burgers_controller");
+
+app.use("/", routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
